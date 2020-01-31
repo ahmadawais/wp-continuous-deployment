cad1d7c19f55e22e8fe33adaeee39799952f2784
Ahmad Awais <me@AhmadAwais.com>
2020-01-31 20:35:12 +0500

    📦 NEW: Welcome

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-/* WP CONTINUOUS DEPLOYMENT */
+#!/usr/bin/env node
 
-/**
- * Wp Continuous Deployment.
- */
-module.exports = () => {};
+// Makes the script crash on unhandled rejections instead of silently
+// ignoring them. In the future, promise rejections that are not handled will
+// terminate the Node.js process with a non-zero exit code.
+process.on("unhandledRejection", err => {
+	console.log("err", err);
+});
+
+const welcome = require("cli-welcome");
+const pkg = require("./package.json");
+
+// Use it.
+welcome(
+	`WP Continous Deployment`,
+	`
+by Awais.dev 👋`,
+	{
+		bgColor: `#FADC00`,
+		color: `#000000`,
+		bold: true,
+		clear: true,
+		version: `v${pkg.version}`
+	}
+);
